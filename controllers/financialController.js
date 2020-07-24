d64aba24b1b7c1eae671808d6d3508af2907a871
andre.machado <4ndre.machado@gmail.com>
2020-07-24 18:03:24 -0300

    Adicionada a possibilidade de filtrar a consulta do GET por período, via query string

--- controllers/financialController.js
+++ controllers/financialController.js
@@ -1,10 +1,17 @@
 const TransactionModel = require('../models/TransactionModel');
 
-//exemplo: http://localhost:3001/api/transaction/
+//obter todas: http://localhost:3001/api/transaction
+//obter uma: http://localhost:3001/api/transaction?period=2020-07
 module.exports.findAll = async (req, res) => {
-  const name = req.query.name;
+  const period = req.query.period; //req.query = após '?'
+
+  //condicao para o filtro no findAll
+  var condition = period
+    ? { yearMonth: { $regex: new RegExp(period), $options: 'i' } }
+    : {};
+
   try {
-    const data = await TransactionModel.find();
+    const data = await TransactionModel.find(condition);
     res.send(data);
     logger.info(`GET /transaction`);
   } catch (error) {
