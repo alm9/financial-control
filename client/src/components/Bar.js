99b858bd5d355e3e53815d0317d1f7f693b843a7
andre.machado <4ndre.machado@gmail.com>
2020-07-25 22:02:35 -0300

    Exibindo e atualizando os valores da barra (Bar) do mês

--- client/src/components/Bar.js
+++ client/src/components/Bar.js
@@ -3,18 +3,46 @@ import { get } from './crud';
 
 export default function Bar({ link }) {
   const [lancamentos, setLancamentos] = useState(0);
+  const [receitas, setReceitas] = useState(0);
+  const [despesas, setDespesas] = useState(0);
+  const [saldo, setSaldo] = useState(0);
 
   useEffect(() => {
-    getLancamento();
+    refreshBar();
     return () => {
       // clearInterval();
     };
   }, [link]);
 
-  const getLancamento = async () => {
-    const quantLancamentos = (await get(link)).length;
-    setLancamentos(quantLancamentos);
+  const refreshBar = async () => {
+    const data = await get(link);
+    let receitas = 0,
+      despesas = 0;
+    data.forEach((item) => {
+      if (item.type === '-') despesas += item.value;
+      else receitas += item.value;
+    });
+    const saldo = receitas - despesas;
+    setLancamentos(data.length);
+    setReceitas(receitas);
+    setDespesas(despesas);
+    setSaldo(saldo);
   };
 
-  return <div>Lançamentos: {lancamentos}</div>;
+  return (
+    <div className="row">
+      <div className="col s12 m6 l3">
+        <p>Lançamentos: {lancamentos}</p>
+      </div>
+      <div className="col s12 m6 l3">
+        <p>Receitas: {receitas}</p>
+      </div>
+      <div className="col s12 m6 l3">
+        <p>Despesas: {despesas}</p>
+      </div>
+      <div className="col s12 m6 l3">
+        <p>Saldo: {saldo}</p>
+      </div>
+    </div>
+  );
 }
